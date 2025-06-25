772be0088832926dab339290fa9343be16426866
nesro <tomas.nesrovnal@windy.com>
2025-06-25 08:49:48 +0200

    feat: allow setting up id via api

--- library.js
+++ library.js
@@ -326,3 +326,22 @@ OAuth.whitelistFields = async (params) => {
 
 	return params;
 };
+
+// This allows setting up the ids via API
+OAuth.updateProfile = async (hookData) => {
+	const { data } = hookData;
+	const { uid } = data;
+
+	if (uid == null || data == null) {
+		return hookData;
+	}
+
+	const names = await db.getSortedSetMembers('oauth2-multiple:strategies');
+	for (const nameId of names.map(name => `${name}Id`)) {
+		if (typeof data[nameId] === 'number') {
+			winston.info(`[plugins/sso-auth0] uid ${uid} setting up ${nameId}=${data[nameId]}, hookData=${JSON.stringify(hookData)}`);
+			await db.setObjectField(`user:${uid}`, nameId, data[nameId]);
+		}
+	}
+	return hookData;
+};
